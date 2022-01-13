ea4ceef5c864b792bee89ea04007eada35d9eff9
Eugene Mereha <eugene.mereha@gmail.com>
2022-01-14 06:41:42 +0700

    добавлены Benchmark метрики

---
+++ src/test/java/stream/ParallelStreamBenchmark.java
@@ -0,0 +1,55 @@
+package stream;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Fork(value = 2, jvmArgs = {"-Xms4G", "-Xmx4G"})
+public class ParallelStreamBenchmark {
+    private static final long N = 10_000_000L;
+
+    @Benchmark
+    public long sequentialSum() {
+        return Stream.iterate(1L, i -> i + 1)
+                .limit(N)
+                .reduce(0L, Long::sum);
+    }
+
+    @Benchmark
+    public long parallelSum() {
+        return Stream.iterate(1L, i -> i + 1)
+                .limit(N)
+                .parallel()
+                .reduce(0L, Long::sum);
+    }
+
+    @Benchmark
+    public long iterativeSum() {
+        long result = 0;
+        for (long i = 1L; i <= N; i++) {
+            result += i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(ParallelStreamBenchmark.class.getSimpleName())
+                .build();
+
+        new Runner(opt).run();
+    }
+
+    @TearDown(Level.Invocation)
+    public void tearDown() {
+        System.gc();
+    }
+}
